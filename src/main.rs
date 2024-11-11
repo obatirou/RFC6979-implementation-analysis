edb3dae238ad70de8de130aff36f41ed53ea9ede
oba <obatirou@gmail.com>
2024-11-11 23:25:57 +0100

    cleaning

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use ecdsa::hazmat::sign_prehashed;
 use elliptic_curve::ops::MulByGenerator;
 use elliptic_curve::point::AffineCoordinates;
 use elliptic_curve::PrimeField;
-use hex as other_hex;
 use hex_literal::hex;
 use k256::{NonZeroScalar, ProjectivePoint, Scalar, Secp256k1};
 use rfc6979::{consts::U32, generate_k};
@@ -33,19 +32,19 @@ fn main() {
 
         let k =
             generate_k::<Sha256, U32>(&private_key.into(), &modulus.into(), &message.into(), b"");
-        println!("k: {}", other_hex::encode(&k));
+        println!("k: {}", hex::encode(&k));
 
         let k = Scalar::from_repr(k.into()).unwrap();
         let k_inv = k.invert();
-        println!("k_inv: {}", other_hex::encode(k_inv.unwrap().to_repr()));
+        println!("k_inv: {}", hex::encode(k_inv.unwrap().to_repr()));
 
         let big_r = ProjectivePoint::mul_by_generator(&k).to_affine();
-        println!("Rx: {}", other_hex::encode(&big_r.x()));
+        println!("Rx: {}", hex::encode(&big_r.x()));
 
         let d = NonZeroScalar::from_repr(private_key.into()).unwrap();
         let (signature, _) = sign_prehashed::<Secp256k1, Scalar>(&d, k, &message.into()).unwrap();
         let (r, s) = signature.split_scalars();
-        println!("r: {}", other_hex::encode(r.to_repr()));
-        println!("s: {}", other_hex::encode(s.to_repr()));
+        println!("r: {}", hex::encode(r.to_repr()));
+        println!("s: {}", hex::encode(s.to_repr()));
     }
 }
